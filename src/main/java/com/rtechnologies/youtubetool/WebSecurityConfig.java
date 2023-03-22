ccb9f78c6bccb6a4f0e9b70a26319085e39e226a
Sam <sameeralii7860@gmail.com>
2023-03-23 00:03:54 +0500

    Third commit

--- src/main/java/com/rtechnologies/youtubetool/WebSecurityConfig.java
+++ src/main/java/com/rtechnologies/youtubetool/WebSecurityConfig.java
@@ -1,17 +1,36 @@
 package com.rtechnologies.youtubetool;
 
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements ApplicationContextAware {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class);
+        http.csrf().disable();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin")
+                .password(encoder.encode("admin"))
+                .roles("ADMIN", "USER")
+                .and()
+                .withUser("user")
+                .password(encoder.encode("password"))
+                .roles("USER");
     }
 }
